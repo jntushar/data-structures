f023b8fff1fbd1c11543746998e4f032353a0cba
jntushar <tjain4736@gmail.com>
2020-10-08 15:34:02 +0530

    stair case dp questions

---
+++ Dynamic Programming/StairCase.java
@@ -0,0 +1,38 @@
+/*
+
+1. You are given a number n, representing the number of stairs in a staircase.
+2. You are on the 0th step and are required to climb to the top.
+3. In one move, you are allowed to climb 1, 2 or 3 stairs.
+4. You are required to print the number of different paths via which you can climb to the top.
+
+*/
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println(getStairCase(n, new int[n + 1]));
+    }
+
+    public static int getStairCase(int n, int[] dp) {
+
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1];
+            } else if (i == 2) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+        }
+        return dp[n];
+    }
+
+}
