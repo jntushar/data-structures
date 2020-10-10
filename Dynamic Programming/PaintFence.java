1c1ac4a344e3a38489809556c299457f4ea814a7
jntushar <tjain4736@gmail.com>
2020-10-10 17:34:02 +0530

    Paint fence

---
+++ Dynamic Programming/PaintFence.java
@@ -0,0 +1,32 @@
+/*
+
+1. You are given a number n and a number k in separate lines, representing the number of fences and number of colors.
+2. You are required to calculate and print the number of ways in which the fences could be painted so that not more than two fences have same colors.
+
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+
+        int same = k;
+        int diff = k * (k-1);
+        int total = same + diff;
+        
+        for(int i=2; i<n; i++){
+            same = diff;
+            diff = total * (k-1);
+            total = same + diff;
+        }
+        
+        System.out.println(total);
+        
+    }
+}
