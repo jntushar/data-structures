7d37672661783106b3439a37b44dece55e440695
jntushar <tjain4736@gmail.com>
2020-11-11 17:37:10 +0530

    Replace node data with sum of larger in BST

--- Binary Search Tree/AddNodeToBST.java
+++ Binary Search Tree/AddNodeToBST.java
@@ -80,7 +80,19 @@ public class Main {
   }
 
   public static Node add(Node node, int data) {
-    // write your code here
+    if (node == null) {
+      return new Node(data, null, null);
+    }
+
+    if (data > node.data) {
+      node.right = add(node.right, data);
+      return node;
+    } else if (data < node.data) {
+      node.left = add(node.left, data);
+      return node;
+    } else {
+      return node;
+    }
   }
 
   public static void main(String[] args) throws Exception {
@@ -105,3 +117,6 @@ public class Main {
   }
 
 }
+
+
+                        
